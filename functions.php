0fb5cc77d66346085e613623a2fa03d6a1bbb6dd
baixiongmao <zxj417870308@gmail.com>
2021-07-29 23:06:15 +0800

    更新项目说明

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 require 'theme-updates/theme-update-checker.php';
 $example_update_checker = new TemeUpdateChecker(
     'pand',
-    ''
-)
+    'https://cdn.jsdelivr.net/gh/baixiongmao/onenav_zi@main/pand.json'
+);
